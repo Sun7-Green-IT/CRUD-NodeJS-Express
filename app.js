dabc6c84cb197cffa7e08efbaafb9bd996d2c42e
Netoun <coulonnier.nicolas.44@gmail.com>
2018-10-10 14:08:40 +0200

    hello

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!')
 })
