8a6cf3bcd12feb45d5e41962482e28db020a1728
Netoun <coulonnier.nicolas.44@gmail.com>
2018-12-07 14:05:54 +0100

    pff

--- src/server.js
+++ src/server.js
@@ -3,17 +3,6 @@ import {
   config
 } from './utils/config'
 import routes from './routes/index'
-// const {
-//   Pool,
-//   Client
-// } = require('pg')
-
-// const connectionString = process.env.URI_PG
-
-
-// const pool = new Pool({
-//   connectionString: connectionString,
-// })
 
 export default function start() {
   const server = express()
@@ -26,20 +15,3 @@ export default function start() {
 
   return server
 }
-
-// app.get('/', (req, res) => {
-//   res.json('Hello World!')
-// })
-
-
-// app.get('/users', (req, res) => {
-//   pool.query('SELECT * FROM users')
-//     .then(rep => {
-//       res.send(rep)
-//     })
-//     .catch(e => console.error(e.stack))
-// })
-
-// app.listen(process.env.PORT || 3000, () => {
-
-// })
